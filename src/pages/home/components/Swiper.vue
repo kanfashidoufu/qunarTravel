1675a3150340bfef6ade6905381fdc78fed290e4
王宁 <wn19950101@126.com>
2018-11-06 15:11:12 +0800

    轮播自动重复

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -18,7 +18,8 @@ export default {
 		return {
 			swiperOption: {
 				pagination: '.swiper-pagination',
-				loop: true
+				loop: true,
+				autoplay: 3000
 			},
 			swiperList: [{
 				id: '0001',
