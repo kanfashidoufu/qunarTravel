c128c4e113b078de97be10b37a001a7021b62e42
王宁 <wn19950101@126.com>
2018-11-06 13:59:08 +0800

    首页轮播组件开发

--- src/main.js
+++ src/main.js
@@ -2,12 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
 import '@/assets/styles/reset.css'
 import '@/assets/styles/border.css'
 import	'@/assets/styles/iconfont.css'
+// require styles
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
   router,
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="wrapper">
+		<swiper :options="swiperOption">
+    	<!-- slides -->
+	    <swiper-slide v-for="item of swiperList" :key="item.id">
+			<img class="swiper-img" :src="item.imgUrl" alt="去哪儿门票">
+	    </swiper-slide>
+	    <!-- Optional controls -->
+	    <div class="swiper-pagination"  slot="pagination"></div>
+	  </swiper>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'HomeSwiper',
+	data () {
+		return {
+			swiperOption: {
+				pagination: '.swiper-pagination',
+				loop: true
+			},
+			swiperList: [{
+				id: '0001',
+				imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/31/da037478f37cf202.jpg_750x200_a02cf862.jpg'
+			},{
+				id: '0002',
+				imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/c6/2467595fffc3b302.jpg_750x200_cca13d51.jpg'
+			}]
+		}
+	}
+}	
+</script>
+
+<style lang="stylus" scoped>
+// >>>表示样式穿透
+	.wrapper >>> .swiper-pagination-bullet-active
+		background: white !important
+	.wrapper
+		overflow: hidden
+		width: 100%
+		height: 0
+		padding-bottom: 27%
+		background: #ccc
+		.swiper-img
+			width: 100%
+</style>
