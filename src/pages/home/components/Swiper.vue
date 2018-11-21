cc3f35717a0060c2a92a49cd69688d8442656ce1
王宁 <wn19950101@126.com>
2018-11-21 10:08:32 +0800

    优化ESlint报错

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,13 +1,13 @@
 <template>
 	<div class="wrapper">
 		<swiper :options="swiperOption" v-if="showSwiper">
-    	<!-- slides -->
-	    <swiper-slide v-for="item of swiperlist" :key="item.id">
-			<img class="swiper-img" :src="item.imgUrl" alt="去哪儿门票">
-	    </swiper-slide>
-	    <!-- Optional controls -->
-	    <div class="swiper-pagination"  slot="pagination"></div>
-	  </swiper>
+            <!-- slides -->
+            <swiper-slide v-for="item of swiperlist" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl" alt="去哪儿门票">
+            </swiper-slide>
+            <!-- Optional controls -->
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
 	</div>
 </template>
 
