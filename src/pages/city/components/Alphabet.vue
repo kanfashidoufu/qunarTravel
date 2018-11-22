aade0955e05fa71309062025113d99d612ca8fe5
王宁 <wn19950101@126.com>
2018-11-22 18:12:10 +0800

    字母表和城市列表之间的联动 兄弟组件间的传值

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,6 +1,17 @@
 <template>
   <ul class="list">
-    <li class="item" v-for="(item, key) of cities" :key="key">{{key}}</li>
+    <li 
+      class="item" 
+      v-for="item of letter" 
+      :key="item"
+      :ref="item" 
+      @click="handleLetterClick" 
+      @touchstart="handleTouchStart" 
+      @touchmove="handleTouchMove" 
+      @touchend="handleTouchEnd"
+    >
+      {{item}}
+    </li>
   </ul>
 </template>
 
@@ -9,6 +20,41 @@ export default {
   name: 'CityAlphabet',
   props: {
     cities: Object
+  },
+  computed: {
+    letter() {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  data() {
+    return {
+      touchStatus: false
+    }
+  },
+  methods: {
+    handleLetterClick: function(e) {
+      this.$emit('change', e.target.innerText)
+    },
+    handleTouchStart: function() {
+      this.touchStatus = true
+    },
+    handleTouchMove: function(e) {
+      if (this.touchStatus) {
+        const startY = this.$refs['A'][0].offsetTop
+        const touchY = e.touches[0].clientY - 79
+        const index = Math.floor((touchY - startY) / 20)
+        if (index >= 0 && index < this.letter.length) {
+          this.$emit('change', this.letter[index])
+        }
+      }
+    },
+    handleTouchEnd: function() {
+      this.touchStatus = false
+    }
   }
 }
 </script>
