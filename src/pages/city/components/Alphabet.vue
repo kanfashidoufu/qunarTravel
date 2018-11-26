5b767a6dd291128362befa68cbf573b4f6e66423
王宁 <wn19950101@126.com>
2018-11-26 17:31:53 +0800

    字母表组件代码性能优化 增加touchmove函数节流

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -32,8 +32,13 @@ export default {
   },
   data() {
     return {
-      touchStatus: false
+      touchStatus: false,
+      startY: 0,
+      timer: null
     }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
   },
   methods: {
     handleLetterClick: function(e) {
@@ -44,12 +49,16 @@ export default {
     },
     handleTouchMove: function(e) {
       if (this.touchStatus) {
-        const startY = this.$refs['A'][0].offsetTop
-        const touchY = e.touches[0].clientY - 79
-        const index = Math.floor((touchY - startY) / 20)
-        if (index >= 0 && index < this.letter.length) {
-          this.$emit('change', this.letter[index])
+        if (this.timer) {
+          clearTimeout(this.timer)
         }
+        this.timer = setTimeout(() => {
+          const touchY = e.touches[0].clientY - 79
+          const index = Math.floor((touchY - this.startY) / 20)
+          if (index >= 0 && index < this.letter.length) {
+            this.$emit('change', this.letter[index])
+          }
+        }, 16)
       }
     },
     handleTouchEnd: function() {
