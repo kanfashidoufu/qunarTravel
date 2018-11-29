3f0c3a756f1ad9df5f12a12118c4d912e57a69b8
王宁 <wn19950101@126.com>
2018-11-29 18:38:20 +0800

    修复scrollTop兼容性问题

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -25,7 +25,7 @@ export default {
   },
   methods: {
     handleScroll () {
-      const top = document.documentElement.scrollTop
+      const top = document.documentElement.scrollTop || document.body.scrollTop || window.pageYOffset
       if (top > 1) {
         let opacity = top / 140
         opacity = opacity > 1 ? 1 : opacity
