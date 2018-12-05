ee8ccb9082345551110812a23e8a69189cfd7578
王宁 <wn19950101@126.com>
2018-12-05 18:34:41 +0800

    解决低端安卓机兼容问题 解决字母表拖动问题

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import fastClick from 'fastclick'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'babel-polyfill'
 import store from './store'
 
 import '@/assets/styles/reset.css'
